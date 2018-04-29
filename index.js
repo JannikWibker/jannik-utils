a42e81f83f5f9162e7dc7f228df665006aa65547
Jannik Wibker <jannikwibker@gmail.com>
2018-04-29 13:09:46 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const datastructures = require('./lib/datatructures.js')
+const dates = require('./lib/dates.js')
+const markdown = require('./lib/markdown.js')
+const other = require('./lib/other.js')
+
+module.exports = {
+  datastructures,
+  dates,
+  markdown,
+  other,
+}
